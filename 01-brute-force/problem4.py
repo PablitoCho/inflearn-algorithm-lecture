9dc1fd92fbe38da35ddd1e57a5b188b578a020fb
woncho <woncho@gmarket.com>
2024-06-26 19:44:15 +0900

    1-5

--- 01-brute-force/problem4.py
+++ 01-brute-force/problem4.py
@@ -13,10 +13,11 @@
 # 489 0 1
 # 2 (왜? 가능한 경우가 324, 328 두 개)
 
-n = int(input()) # 4
-hints = [list(map(int, input().split())) for _ in range(n)] # [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]
+## 내 답
+# n = int(input()) # 4
+# hints = [list(map(int, input().split())) for _ in range(n)] # [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]
 
-def strike(num1, num2):
+def get_strike(num1, num2):
     strikes = 0
     strnum1 = str(num1)
     for i, num in enumerate(str(num2)):
@@ -24,7 +25,7 @@ def strike(num1, num2):
             strikes += 1
     return strikes
 
-def ball(num1, num2):
+def get_ball(num1, num2):
     balls = 0
     strnum1 = str(num1)
     for i, num in enumerate(str(num2)):
@@ -32,22 +33,40 @@ def ball(num1, num2):
             balls += 1
     return balls
 
+# answer = 0
+# for i in range(100, 1000):
+#     flags = []
+#     for hint in hints:
+#         num, strickes, balls = hint
+#         flags.append(
+#             strike(num, i) == strickes and ball(num, i) == balls)
+#     if all(flags):
+#         answer += 1
+
+# print(answer)
+
+n = int(input())
+
+hint = [list(map(int, input().split())) for _ in range(n)]
+
 answer = 0
-for i in range(100, 1000):
-    flags = []
-    for hint in hints:
-        num, strickes, balls = hint
-        flags.append(
-            strike(num, i) == strickes and ball(num, i) == balls)
-    if all(flags):
-        answer += 1
+for a in range(1, 10): # 100 자리수
+    for b in range(10): # 10의 자리수
+        for c in range(10): # 1의 자리수
+            if (a == b or b == c or c == a): # 중복된 수가 있는 경우는 숫자 야구 자체가 성립하지 않는다
+                continue
+            
+            cnt = 0
+            for arr in hint:
+                number, ball, strike = arr
+                
+                target = a * 100 + b * 10 + c
+                ball_count = get_ball(target, number)
+                strike_count = get_strike(target, number)
+
+                if ball == ball_count and strike == strike_count:
+                    cnt += 1
+            if cnt == n:
+                answer += 1
 
 print(answer)
-
-
-# if __name__ == "__main__":
-#     num1 = 324
-#     num2 = 241
-#     strikes = strike(num1, num2)
-#     balls = ball(num1, num2)
-#     print(strikes, balls)
--- 01-brute-force/problem5.py
+++ 01-brute-force/problem5.py
@@ -14,3 +14,42 @@
 # 16 15 - D 유리
 # 0 2 3 4
 
+# 방법1)
+# 모든 위치에서 모든 친구들의 거리를 계산해서 가장 적은 값을 알려준다
+# X, Y 각각 계산한 후, 합쳐서 최솟값을 알려준다
+
+# 방법2)
+# 우리가 한 곳에서 모일 때, 비용을 최소화하기 위해서는 우리의 집 중 한곳에서 모이면 된다.
+
+n = int(input()) # n
+coords = [list(map(int, input().split())) for _ in range(n)]
+# print(n, coords)
+
+Xs = set([coord[0] for coord in coords])
+Ys = set([coord[1] for coord in coords])
+
+def cal_distance(coord1, coord2):
+    cal = abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
+    print(f'  calculated {cal}')
+    return cal
+
+curr = 1000
+found = []
+for x in Xs:
+    for y in Ys:
+        dist = 0
+        for c in zip(Xs, Ys):
+            dist += cal_distance((x,y), c)
+        print(f"x {x} y {y} dist {dist}")
+        if dist < curr:
+            curr = dist
+            found = (x, y)
+
+print(curr, found)
+
+# if __name__ == "__main__":
+#     a = [1,2,3]
+#     b = [4,5,6]
+#     for v in zip(a,b):
+#         print(v)
+
